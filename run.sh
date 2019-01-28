ed973d5838c372dd79d48dbbaa5729fe22d41383
Rahul Prasad <therahulprasad@users.noreply.github.com>
2019-01-28 14:38:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 sudo apt update
-sudo apt -y install git
+sudo apt -y install git curl vim
+
+# Configure terminal from https://github.com/therahulprasad/my-terminal-configuration
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/therahulprasad/my-terminal-configuration/master/init.sh)"
+
+# Configure vim from https://github.com/therahulprasad/my-vim-configulation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/therahulprasad/my-vim-configulation/master/install.sh)"
