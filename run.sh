f934015d3197a5c492926ab94b67998bbd4413d8
Rahul Prasad <therahulprasad@users.noreply.github.com>
2019-02-04 18:21:06 +0530

    install adb

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ $TODO == "setup-base" ]; then
   ls $HOME/Development/go || mkdir $HOME/Development/go
 
   sudo apt update
-  sudo apt -y install git curl vim snapd python3-pip   
+  sudo apt -y install git curl vim snapd python3-pip adb  
 
   # Configure terminal from https://github.com/therahulprasad/my-terminal-configuration
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/therahulprasad/my-terminal-configuration/master/init.sh)"
